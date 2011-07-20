d4da9b334776880751c8c43b5ff6778e80b45716
contact@randomwalks.org <contact@randomwalks.org>
2011-07-20 19:27:21 +0900

    update

--- public/javascripts/welcomes/index.js
+++ public/javascripts/welcomes/index.js
@@ -1,11 +1,14 @@
 var welcomeController = {
   welcomes: null,
   length: null,
+  basicWidth: 0,
   currentIndex: 0,
   intervalId: null,
   init: function(){
     this.welcomes = $(".welcome");
     this.length = this.welcomes.length;
+    
+    
     this.welcomes.hover(function(e){
       $(this).find(".welcomeback").fadeOut(200);
       $(this).find(".detailBtn").animate({backgroundColor:'#434343'});
@@ -13,11 +16,22 @@ var welcomeController = {
       $(this).find(".welcomeback").fadeIn(200);
       $(this).find(".detailBtn").animate({backgroundColor:'#FCF242'});
     });
+    
     this.welcomes.click(function(e){
       e.preventDefault();
-      document.location.href=$(this).find(".detailBtn a").attr("href");
+      document.location.href = $(this).find(".detailBtn a").attr("href");
     });
+  
+    this.basicWidth = 960 / this.welcomes.length;
+    
+    this.welcomes.each($.proxy(this.setWidth, this));
+    
   },
+  
+  setWidth: function(index, item){
+    $(item).css('width', this.basicWidth + 'px');    
+  },
+  
   loadStart: function(){
     this.intervalID = setInterval($.proxy(this.load, this), 300);
   },
@@ -31,6 +45,8 @@ var welcomeController = {
     }
   }
 };
+
+
 $(document).ready(function(e){
   welcomeController.init();
   welcomeController.loadStart();
