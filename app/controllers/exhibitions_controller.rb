f40b584bb60d368c6c760a1ebc2104d664133bc0
contact@randomwalks.org <contact@randomwalks.org>
2011-07-29 14:09:03 +0900

    update

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -6,14 +6,17 @@ class ExhibitionsController < ApplicationController
     @exhibitions = {}
     
     @exs = Exhibition.where("current = ? AND upcoming = ?", false, false).order("start_date DESC")
+    @years = []
     @exs.each do |ex|
       year = ex.start_date.strftime("%Y")
+      @years << year.to_i
       if @exhibitions[year.to_s] == nil
         @exhibitions[year.to_s] =[]
       end
       @exhibitions[year.to_s] << ex
     end
-    logger.info @exhibitions
+    
+    @years.sort!
   end
   
   
