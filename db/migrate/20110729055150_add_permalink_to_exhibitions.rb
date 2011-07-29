11eb61c0a6f4e712c193da1ec4f87072bf72ee00
contact@randomwalks.org <contact@randomwalks.org>
2011-07-29 15:20:56 +0900

    update

---
+++ db/migrate/20110729055150_add_permalink_to_exhibitions.rb
@@ -0,0 +1,13 @@
+class AddPermalinkToExhibitions < ActiveRecord::Migration
+  def self.up
+    add_column :exhibitions, :permalink, :string
+    add_column :educations, :permalink, :string
+    add_column :events, :permalink, :string
+  end
+
+  def self.down
+    remove_column :exhibitions, :permalink
+    remove_column :educations, :permalink
+    remove_column :events, :permalink
+  end
+end
