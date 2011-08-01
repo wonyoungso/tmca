c671543d1025c15982a0a7840da3370d04441051
contact@randomwalks.org <contact@randomwalks.org>
2011-08-01 21:24:57 +0900

    update

---
+++ db/migrate/20110801121303_create_pdfs.rb
@@ -0,0 +1,15 @@
+class CreatePdfs < ActiveRecord::Migration
+  def self.up
+    create_table :pdfs do |t|
+      t.integer :attachment_file_size
+      t.string :attachment_file_name
+      t.string :attachment_content_type
+      t.datetime :attachment_updated_at
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :pdfs
+  end
+end
