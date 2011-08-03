e3176617d0d5034ba07eb48028c0a7a079265612
contact@randomwalks.org <contact@randomwalks.org>
2011-08-03 20:16:35 +0900

    update

---
+++ db/migrate/20110803110601_add_category_id_to_exhibitions.rb
@@ -0,0 +1,9 @@
+class AddCategoryIdToExhibitions < ActiveRecord::Migration
+  def self.up
+    add_column :exhibitions, :category_id, :integer
+  end
+
+  def self.down
+    remove_column :exhibitions, :category_id
+  end
+end
