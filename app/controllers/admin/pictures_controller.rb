ed5063fb98b06cff139b51c3876a38813880402f
contact@randomwalks.org <contact@randomwalks.org>
2011-08-03 18:21:45 +0900

    update

--- app/controllers/admin/pictures_controller.rb
+++ app/controllers/admin/pictures_controller.rb
@@ -1,8 +1,32 @@
 # -*- encoding : utf-8 -*-
 class Admin::PicturesController < ApplicationController
   def create
-  end
+
+       @picture = Picture.new
+       @picture.photo = params[:file]
+       @picture.pictureable_id = params[:pictureable_id]
+       @picture.pictureable_type = params[:pictureable_type]
+       if @picture.save
+         respond_to do |format|
+           format.json {render :json => {:success => true, :message => '그림 업로드가 완료되었습니다.', :picture => picture_as_json(@picture)}}
+         end
+       else
+         respond_to do |format|
+           format.json {render :json => {:success => false, :message => '그림 업로드가 실패하였습니다.'}}
+         end
+       end
+
+   end
+
+
+   def picture_as_json(picture)
+     picture = {
+       :id => picture.id,
+       :url => picture.photo.url,
+       :file_name => picture.photo_file_name
+     }
+     picture
+   end
+
   
-  def destroy
-  end
 end
---
+++ db/migrate/20110803090559_add_attachment_to_news.rb
@@ -0,0 +1,16 @@
+class AddAttachmentToNews < ActiveRecord::Migration
+  def self.up
+    add_column :news, :attachment_file_size, :integer
+    add_column :news, :attachment_content_type, :string
+    add_column :news, :attachment_file_name, :string
+    add_column :news, :attachment_updated_at, :datetime
+  end
+
+  def self.down
+
+      remove_column :news, :attachment_file_size
+      remove_column :news, :attachment_content_type
+      remove_column :news, :attachment_file_name
+      remove_column :news, :attachment_updated_at
+  end
+end
