5adb3d0355ca95b3a9f08af53fdd04c5582be619
bizarre <b@tumblbug.com>
2011-08-15 13:45:27 +0900

    update

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,8 +11,8 @@ class SearchController < ApplicationController
       @news = News.where(:created_at => Date(numeric, 1, 1)..Date(numeric, 12, 31))
         
     else
-      @exhibitions = Exhibition.where("title LIKE ? or description LIKE ?", params[:query])
-      @news = News.where("title LIKE ? or description LIKE ?", params[:query])
+      @exhibitions = Exhibition.where("title LIKE ? or description LIKE ?", "%#{params[:query]}%")
+      @news = News.where("title LIKE ? or description LIKE ?", "%#{params[:query]}%")
     end
     
     @size = @exhibitions.size + @news.size
