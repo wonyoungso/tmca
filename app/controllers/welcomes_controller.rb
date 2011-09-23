fbeaf117777a58a2ea16e6638caecec091bf9adf
bizarre <b@tumblbug.com>
2011-09-23 22:14:33 +0900

    update

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,8 +1,6 @@
 # -*- encoding : utf-8 -*-
 class WelcomesController < ApplicationController
   def index
-    @current_exhibition = Exhibition.where("current = ? AND category_id = ?", true, 1).first
-    @current_education = Exhibition.where("current = ? AND category_id = ?", true, 2).first
-    @current_event = Exhibition.where("current = ? AND category_id = ?", true, 3).first
+    @currents = Current.order("idex ASC")
   end
 end
