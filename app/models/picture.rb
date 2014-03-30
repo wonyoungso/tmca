7cf7a050d8cf2864c1c938256068638cdfc2d8c7
bizarre <b@tumblbug.com>
2014-03-30 14:57:17 +0900

    update

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,12 @@
 # -*- encoding : utf-8 -*-
 class Picture < ActiveRecord::Base
-  has_attached_file :photo, :styles => {:main => "320x520", :medium => '320x500#', :thumb => '100x100#'}, :dependent => :destroy 
+  has_attached_file :photo, :styles => {
+    :main => "320x520", 
+    :medium => '320x500#', 
+    :thumb => '100x100#'
+  }, 
+  :path => 'photos/:id/:style/:filename',
+  :dependent => :destroy 
    
   belongs_to :pictureable, :polymorphic => true
 end
