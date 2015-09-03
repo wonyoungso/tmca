e15a33a19c26dda0b3c6f6d12a356ac9c83fbea6
bizarre <b@tumblbug.com>
2015-09-03 18:20:16 +0900

    update

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -5,7 +5,10 @@ class Pdf < ActiveRecord::Base
   :path => 'public/system/attachments/:id/:style/:filename',
   :dependent => :destroy
 
-  validates_attachment_content_type :attachment, :content_type => ["application/unknown", "application/x-hwp", "application/haansofthwp", "application/vnd.hancom.hwp", "application/pdf", "application/octet-stream"]
+  # validates_attachment_content_type :attachment, :content_type => ["application/unknown", "application/x-hwp", "application/haansofthwp", "application/vnd.hancom.hwp", "application/pdf", "application/octet-stream"]
   
+
+  do_not_validate_attachment_file_type :attachment
+
   belongs_to :pdfable, :polymorphic => true
 end
